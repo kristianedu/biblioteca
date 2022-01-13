b5bcb02aa3331739212c83e9a481c4ccd2a1c175
kristianedu <71871896+kristianedu@users.noreply.github.com>
2022-01-12 18:14:13 -0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn = mysqli_connect(
+  'mysql-libritos.alwaysdata.net',
+  'libritos',
+  'secomoroberto',
+  'libritos_biblioteca'
+) or die(mysqli_erro($mysqli));
+
+
+?>
---
+++ validar.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+include('db.php');
+include('Clases/classprestamo.php');
+
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+
+$tipo = 0;
+$id =0;
+$user = "";
+$consulta="SELECT*FROM usuarios where User='$usuario' and Passwd='$contraseña'";
+$resultado=mysqli_query($conn,$consulta);
+
+
+foreach ($resultado as $opciones):
+  $_SESSION['auth_id'] = $opciones['ID'];
+  $_SESSION['auth_user'] = $opciones['User'];
+  $_SESSION['auth_tipo'] = $opciones['tipo'];
+  $tipo = $opciones['tipo'];
+  endforeach;
+
+if($tipo == 1){
+  header('Location: admin/inicio.php ');
+  
+}if($tipo == 2){
+
+$consulta="SELECT fechaF,IDPrestamo FROM prestasmo";
+$resultado=mysqli_query($conn,$consulta);
+
+foreach ($resultado as $opciones):
+prosEstado($opciones['IDPrestamo'],$opciones['fechaF']);
+endforeach;
+
+header('Location: empleado/listarusuarios.php ');
+ 
+}if($tipo == 3|| $tipo == 4 ){
+  header('Location: user/infouser.php ');
+  
+}else{
+    ?>
+    <?php
+    include("index.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conn);
